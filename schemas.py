130ffe262c262a56d10f318acec0a73789a8a740
mohamedbaker <dam.vampire22@gmail.com>
2024-09-20 03:49:39 +0300

    adding tags to store and items and implementing it endpoiints

--- schemas.py
+++ schemas.py
@@ -2,13 +2,18 @@ from marshmallow import Schema, fields
 
 
 class PlainItemSchema(Schema):
-    id = fields.Str(dump_only=True)
+    id = fields.Int(dump_only=True)
     name = fields.Str(required=True)
     price = fields.Float(required=True)
 
 class PlainStoreSchema(Schema):
-    id = fields.Str(dump_only=True)
+    id = fields.Int(dump_only=True)
     name = fields.Str(required=True)
+
+class plainTagSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str()
+
 
 class ItemUpdateSchema(Schema):
     name = fields.Str()
@@ -21,3 +26,14 @@ class ItemSchema(PlainItemSchema):
 
 class StoreSchema(PlainStoreSchema):
     items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
+    tags = fields.List(fields.Nested(plainTagSchema()), dump_only=True)
+    
+class TagSchema(plainTagSchema):
+    store_id = fields.Int(load_only=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
+    items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
+
+class TagAndItemSchema(Schema):
+    message = fields.Str()
+    item = fields.Nested(ItemSchema)
+    tag = fields.Nested(TagSchema)
