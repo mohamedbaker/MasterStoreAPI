8347afe48ec5aa88d6c8d74b1f991d83d449f63e
mohamedbaker <dam.vampire22@gmail.com>
2024-09-20 05:23:18 +0300

    add jwt auth ,blocklist,and logout

--- app.py
+++ app.py
@@ -2,14 +2,23 @@ import os
 
 from flask import Flask
 from flask_smorest import Api
+from flask_jwt_extended import (
+    create_access_token,
+    get_jwt,
+    jwt_required,
+)
+
+from blocklist import BLOCKLIST
 
 from db import db
+from blocklist import BLOCKLIST
 
 import models
 
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
 from resources.tag import blp as TagBlueprint
+from resources.user import blp as UserBlueprint
 
 
 def create_app(db_url=None):
@@ -27,6 +36,65 @@ def create_app(db_url=None):
     app.config["PROPAGATE_EXCEPTIONS"] = True
     db.init_app(app)
     api = Api(app)
+    
+    app.config["JWT_SECRET_KEY"] = "112085364303668303741437337820239986161"
+    jwt = JWTManager(app)
+    
+    @jwt.token_in_blocklist_loader
+    def check_if_token_in_blocklist(jwt_header, jwt_payload):
+        return jwt_payload["jti"] in BLOCKLIST
+
+
+    @jwt.revoked_token_loader
+    def revoked_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify(
+                {"description": "The token has been revoked.", "error": "token_revoked"}
+            ),
+            401,
+        )
+    
+    @jwt.additional_claims_loader
+    def add_claims_to_jwt(identity):
+        if identity == 1:
+            return {"is_admin": True}
+        return {"is_admin": False}
+    
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify({"message": "The token has expired.", "error": "token_expired"}),
+            401,
+        )
+
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return (
+            jsonify(
+                {"message": "Signature verification failed.", "error": "invalid_token"}
+            ),
+            401,
+        )
+        
+    @blp.route("/logout")
+    class UserLogout(MethodView):
+        @jwt_required()
+        def post(self):
+            jti = get_jwt()["jti"]
+            BLOCKLIST.add(jti)
+            return {"message": "Successfully logged out"}, 200
+
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return (
+            jsonify(
+                {
+                "description": "Request does not contain an access token.",
+                "error": "authorization_required",
+                 }
+            ),
+            401,
+        )
 
     with app.app_context():
         db.create_all()
@@ -34,5 +102,6 @@ def create_app(db_url=None):
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
     api.register_blueprint(TagBlueprint)
+    api.register_blueprint(UserBlueprint)
 
     return app
